a1f40f0659d7fe00ef071ce2ca859bfa1fc02490
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2024-02-10 18:07:53 +0200

    layout register page

--- mushrooms-ui/src/App.jsx
+++ mushrooms-ui/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Authorization/Login';
+import SignUp from './pages/Authorization/SignUp';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/sign-in" element={<Login />} />
+				<Route path="/sign-up" element={<SignUp />} />
 				<Route path="*" element={<></>} />
 			</Routes>
 		</>
