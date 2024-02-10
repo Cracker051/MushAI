62a5471ef9c8df030ccb133ef849d7fa3bfc8688
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2024-02-10 17:51:52 +0200

    layout sign in page

--- mushrooms-ui/src/App.jsx
+++ mushrooms-ui/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Authorization/Login';
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<></>} />
+				<Route path="/sign-in" element={<Login />} />
+				<Route path="*" element={<></>} />
 			</Routes>
 		</>
 	);
---
+++ mushrooms-ui/src/components/AuthSubmitButton.jsx
@@ -0,0 +1,9 @@
+const AuthSubmitButton = (props) => {
+	return (
+		<button
+			{...props}
+			className="py-4 text-3xl font-extrabold transition-colors rounded-sm select-none text-stone-900 hover:bg-stone-200/60 bg-stone-300"></button>
+	);
+};
+
+export default AuthSubmitButton;
