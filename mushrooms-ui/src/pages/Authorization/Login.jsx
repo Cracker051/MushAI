9a99600ec280ffe1e93cc08ab5932d8833a5b626
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2024-02-10 18:07:23 +0200

    fix form width

--- mushrooms-ui/src/pages/Authorization/Login.jsx
+++ mushrooms-ui/src/pages/Authorization/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
 		<>
 			<MainHeader />
 			<main className="flex items-center justify-center min-h-screen text-stone-100 bg-stone-900">
-				<form className="flex flex-col gap-5 p-4">
+				<form className="flex flex-col gap-5 p-4 w-[32rem]">
 					<h2 className="text-2xl font-black">AUTHORIZATION</h2>
 					<h3 className="text-2xl font-bold">LOG IN TO VIEW HISTORY AND POST</h3>
 					<div className="flex flex-col gap-5 my-4">
