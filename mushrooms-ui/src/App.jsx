52e235097b1bd9e33a8b2d17226c361923a0bac4
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2024-02-11 12:46:51 +0200

    layout home page

--- mushrooms-ui/src/App.jsx
+++ mushrooms-ui/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Login from './pages/Authorization/Login';
 import SignUp from './pages/Authorization/SignUp';
 import RecoverPassword from './pages/Authorization/RecoverPassword';
+import Home from './pages/Home';
 
 function App() {
 	return (
 		<>
 			<Routes>
+				<Route path="/" element={<Home />} />
 				<Route path="/sign-in" element={<Login />} />
 				<Route path="/sign-up" element={<SignUp />} />
 				<Route path="/recover-password" element={<RecoverPassword />} />
