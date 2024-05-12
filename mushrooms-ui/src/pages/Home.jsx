1c715435515d46d6e405863a452efcca1c248729
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2024-05-13 02:42:02 +0300

    home page improvings

--- mushrooms-ui/src/pages/Home.jsx
+++ mushrooms-ui/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import PicSection1 from '../assets/home_section1_mush.jpg';
 import PicSection2AiSearch from '../assets/icon_home_aisearch.svg';
 import PicSection2Community from '../assets/icon_home_community.svg';
 import PicSection2Blog from '../assets/icon_home_blog.svg';
+import { useGetPostedBlogs } from '../state/server/blog/useGetBlogs';
+
+const BACKEND_URL = import.meta.env.VITE_APP_API_URL;
 
 const Home = () => {
+	const navigate = useNavigate();
+	const postedBlogsQuery = useGetPostedBlogs({ page: 1, size: 2 });
 	return (
 		<>
 			<section className="bg-msh-dark text-msh-light">
@@ -16,23 +21,25 @@ const Home = () => {
 							<img src={PicSection1} alt="" className="w-full rounded-xl" />
 						</div>
 						<div className="flex flex-col gap-4 xl:gap-8">
-							<div className="flex gap-6 max-sm:flex-wrap text-msh-dark">
-								<div className="flex flex-col justify-center w-full h-40 gap-4 p-2 md:w-48 xl:gap-8 xl:h-72 xl:w-[23rem] bg-msh-light rounded-xl">
-									<p className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] text-2xl font-medium xl:text-4xl">
-										&quot;Mushrooms in medicine&quot;
-									</p>
-									<button className="hover:bg-opacity-80 shadow-[0_4px_4px_rgba(0,0,0,0.5)] self-end p-1 lg:p-2 text-2xl rounded-lg bg-msh-dark text-stone-200">
-										Read
-									</button>
-								</div>
-								<div className="flex flex-col justify-center w-full h-40 gap-4 p-2 md:w-48 xl:gap-8 xl:h-72 xl:w-[23rem] bg-msh-light rounded-xl">
-									<p className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] text-2xl font-medium xl:text-4xl">
-										&quot;How to pick mushrooms&quot;
-									</p>
-									<button className="hover:bg-opacity-80 shadow-[0_4px_4px_rgba(0,0,0,0.5)] self-end p-1 lg:p-2 text-2xl rounded-lg bg-msh-dark text-stone-200">
-										Read
-									</button>
-								</div>
+							<div className="flex gap-3 xl:gap-6 max-sm:flex-wrap text-msh-light">
+								{postedBlogsQuery.data?.items.map((blog) => (
+									<div
+										key={blog.id}
+										style={{ backgroundImage: `url(${BACKEND_URL + `/${blog.icon}`})` }}
+										className="flex flex-col justify-between w-full h-40 gap-2 p-2 md:w-48 xl:gap-8 xl:h-72 xl:w-[23rem] bg-cover bg-center bg-msh-light rounded-xl">
+										<p className="p-1 text-xl font-medium text-center rounded-md xl:p-2 drop-shadow bg-msh-dark/40 xl:text-4xl">
+											{blog.title}
+										</p>
+										<Link
+											to={`/blog/${blog.id}`}
+											className="hover:bg-opacity-50 transition-all shadow-[0_4px_4px_rgba(0,0,0,0.5)] self-end p-1 lg:p-2 text-md xl:text-2xl rounded-lg bg-msh-dark text-stone-200">
+											Read
+										</Link>
+										<p className="p-1 font-medium text-center uppercase rounded-md xl:p-2 xl:text-xl drop-shadow bg-msh-dark/40">
+											{blog.user.name} {blog.user.surname}
+										</p>
+									</div>
+								))}
 							</div>
 							<div className="max-lg:self-center">
 								<p className="text-3xl xl:text-4xl">
@@ -47,7 +54,9 @@ const Home = () => {
 						</div>
 					</div>
 					<div className="flex items-center gap-4 my-10 max-sm:flex-wrap-reverse max-sm:justify-center max-sm:text-center">
-						<button className="px-8 py-2 text-5xl font-medium leading-none text-black transition-colors lg:text-6xl rounded-2xl bg-msh-light hover:bg-stone-300/80">
+						<button
+							onClick={() => navigate('/community')}
+							className="px-8 py-2 text-5xl font-medium leading-none text-black transition-colors lg:text-6xl rounded-2xl bg-msh-light hover:bg-stone-300/80">
 							Start
 						</button>
 						<p className="text-2xl">
@@ -74,7 +83,9 @@ const Home = () => {
 						</Link>
 					</div>
 					<div className="flex flex-wrap justify-center gap-6 mt-12 text-msh-light">
-						<div className="p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
+						<Link
+							to={'/search'}
+							className="hover:scale-105 hover:opacity-90 transition-all p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
 							<h4 className="flex items-center gap-4 text-4xl font-medium">
 								<img src={PicSection2AiSearch} alt="" />
 								AI Search
@@ -83,8 +94,10 @@ const Home = () => {
 								AI mushroom recognition feature! Simply upload an image, and let our advanced
 								algorithms work their magic.
 							</p>
-						</div>
-						<div className="p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
+						</Link>
+						<Link
+							to={'/community'}
+							className="hover:scale-105 hover:opacity-90 transition-all p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
 							<h4 className="flex items-center gap-4 text-4xl font-medium">
 								<img src={PicSection2Community} alt="" />
 								Community
@@ -92,8 +105,10 @@ const Home = () => {
 							<p className="text-3xl">
 								Discover our Mushroom Community – your hub for all things fungi!
 							</p>
-						</div>
-						<div className="p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
+						</Link>
+						<Link
+							to={'/blog/?tab=all'}
+							className="hover:scale-105 hover:opacity-90 transition-all p-6 pb-12 space-y-4 shadow-[0_0_30px_rgba(28,25,23,0.8)] rounded-2xl bg-msh-dark lg:max-w-96">
 							<h4 className="flex items-center gap-4 text-4xl font-medium">
 								<img src={PicSection2Blog} alt="" />
 								Blog
@@ -101,7 +116,7 @@ const Home = () => {
 							<p className="text-3xl">
 								Search or post texts about mushrooms in a vibrant space dedicated to enthusiasts.
 							</p>
-						</div>
+						</Link>
 					</div>
 				</div>
 			</section>
